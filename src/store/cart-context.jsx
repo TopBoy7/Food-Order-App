c388f3c9d2fc873fb210d8c11ac767f5702df991
TopBoy7 <okefejoseph9@gmail.com>
2023-09-07 01:24:16 +0100

    ninthCommit

--- src/store/cart-context.jsx
+++ src/store/cart-context.jsx
@@ -1,7 +1,10 @@
-import React from "react"
+import React from 'react';
 
-React.createContext({
+const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
-  addItem: () => {}
-})
+  addItem: (item) => {},
+  removeItem: (id) => {}
+});
+
+export default CartContext;
